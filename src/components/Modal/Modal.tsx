602b3dab2029337053352e1645fe79c683d97113
NataliiaValko <75252511+NataliiaValko@users.noreply.github.com>
2025-08-11 13:37:35 +0300

    add children to Modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,6 +1,6 @@
 import css from "./Modal.module.css";
 
-export default function Modal() {
+export default function Modal({ children }) {
   <div className={css.backdrop} role="dialog" aria-modal="true">
     <div className={css.modal}>{children}</div>
   </div>;

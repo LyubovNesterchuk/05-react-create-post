44f949e932924880fe18f94cea180ff254a33ae3
LyubovNesterchuk <nesterchuklyubov@gmail.com>
2025-08-16 08:15:12 +0300

    update

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,113 @@
+// import { useState } from "react";
+// import { keepPreviousData, useQuery } from "@tanstack/react-query";
+// import { useDebouncedCallback } from "use-debounce";
+
+// import Modal from "../Modal/Modal";
+// import PostList from "../PostList/PostList";
+// import SearchBox from "../SearchBox/SearchBox";
+// import Pagination from "../Pagination/Pagination";
+// import CreatePostForm from "../CreatePostForm/CreatePostForm";
+// import EditPostForm from "../EditPostForm/EditPostForm";
+
+// import { fetchPosts } from "../../services/postService";
+// import type { Post } from "../../types/post";
+// import css from "./App.module.css";
+
+// export default function App() {
+//   const [currentPage, setCurrentPage] = useState(1);
+//   const [searchQuery, setSearchQuery] = useState("");
+//   const [debouncedSearchQuery, setDebouncedSearchQuery] = useState("");
+//   const [isModalOpen, setIsModalOpen] = useState(false);
+//   const [isCreatePost, setIsCreatePost] = useState(false);
+//   const [isEditPost, setIsEditPost] = useState(false);
+//   const [editedPost, setEditedPost] = useState<Post | null>(null);
+
+ 
+//   const debounced = useDebouncedCallback((value: string) => {
+//     setDebouncedSearchQuery(value);
+//     setCurrentPage(1);
+//   }, 300);
+
+//   const handleSearch = (value: string) => {
+//     setSearchQuery(value);
+//     debounced(value);
+//   };
+
+//   const openCreateModal = () => {
+//     setIsCreatePost(true);
+//     setIsEditPost(false);
+//     setEditedPost(null);
+//     setIsModalOpen(true);
+//   };
+
+//   const openEditModal = () => {
+//     setIsEditPost(true);
+//     setIsCreatePost(false);
+//     setIsModalOpen(true);
+//   };
+
+//   const selectPostForEdit = (post: Post) => {
+//     setEditedPost(post);
+//   };
+
+//   const closeModal = () => {
+//     setIsModalOpen(false);
+//     setIsCreatePost(false);
+//     setIsEditPost(false);
+//     setEditedPost(null);
+//   };
+
+//   const { data, isLoading } = useQuery({
+//     queryKey: ["posts", debouncedSearchQuery, currentPage],
+//     queryFn: () =>
+//       fetchPosts({
+//         searchText: debouncedSearchQuery,
+//         page: currentPage,
+//         limit: 10,
+//       }),
+//     placeholderData: keepPreviousData,
+//   });
+
+//   if (isLoading) {
+//     return <div className={css.loader}>Loading...</div>;
+//   }
+
+//   const posts = data || [];
+
+//   const totalPages = posts.length > 0 ? 10 : 0;
+
+//   return (
+//     <div className={css.app}>
+//       <header className={css.toolbar}>
+//         <SearchBox value={searchQuery} onChange={handleSearch} />
+//         {totalPages > 1 && (
+//           <Pagination
+//             currentPage={currentPage}
+//             totalPages={totalPages}
+//             onPageChange={setCurrentPage}
+//           />
+//         )}
+//         <button className={css.button} onClick={openCreateModal}>
+//           Create post
+//         </button>
+//       </header>
+
+//       <Modal isOpen={isModalOpen} onClose={closeModal}>
+//         {isCreatePost && <CreatePostForm onClose={closeModal} />}
+//         {isEditPost && editedPost && (
+//           <EditPostForm initialValues={editedPost} onClose={closeModal} />
+//         )}
+//       </Modal>
+
+//       <PostList
+//         posts={posts}
+//         toggleModal={openEditModal}
+//         toggleEditPost={selectPostForEdit}
+//       />
+//     </div>
+//   );
+// }
+
 import { useState } from "react";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { useDebouncedCallback } from "use-debounce";
@@ -11,62 +121,51 @@ import EditPostForm from "../EditPostForm/EditPostForm";
 
 import { fetchPosts } from "../../services/postService";
 import type { Post } from "../../types/post";
+
 import css from "./App.module.css";
 
 export default function App() {
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [searchQuery, setSearchQuery] = useState<string>("");
 
-  const [currentPage, setCurrentPage] = useState(1);
-  const [searchQuery, setSearchQuery] = useState("");
-  const [debouncedSearchQuery, setDebouncedSearchQuery] = useState("");
-
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isCreatePost, setIsCreatePost] = useState(false);
-  const [isEditPost, setIsEditPost] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [isCreatePost, setIsCreatePost] = useState<boolean>(false);
+  const [isEditPost, setIsEditPost] = useState<boolean>(false);
   const [editedPost, setEditedPost] = useState<Post | null>(null);
 
+  const debouncedSearchQuery = useDebouncedCallback(
+    (value: string) => setSearchQuery(value),
+    300
+  );
 
-  const debounced = useDebouncedCallback((value: string) => {
-    setDebouncedSearchQuery(value);
-    setCurrentPage(1);
-  }, 300);
-
-  const handleSearch = (value: string) => {
-    setSearchQuery(value);
-    debounced(value);
-  };
-
-  const openCreateModal = () => {
+  const openCreateModal = (): void => {
     setIsCreatePost(true);
     setIsEditPost(false);
     setEditedPost(null);
     setIsModalOpen(true);
   };
 
-  const openEditModal = () => {
+  const openEditModal = (): void => {
     setIsEditPost(true);
     setIsCreatePost(false);
     setIsModalOpen(true);
   };
 
-  const selectPostForEdit = (post: Post) => {
+  const selectPostForEdit = (post: Post): void => {
     setEditedPost(post);
   };
 
-  const closeModal = () => {
+  const closeModal = (): void => {
     setIsModalOpen(false);
     setIsCreatePost(false);
     setIsEditPost(false);
     setEditedPost(null);
   };
 
-  const { data, isLoading } = useQuery({
-    queryKey: ["posts", debouncedSearchQuery, currentPage],
+  const { data = [], isLoading } = useQuery<Post[]>({
+    queryKey: ["posts", searchQuery, currentPage],
     queryFn: () =>
-      fetchPosts({
-        searchText: debouncedSearchQuery,
-        page: currentPage,
-        limit: 10,
-      }),
+      fetchPosts({ searchText: searchQuery, page: currentPage, limit: 10 }),
     placeholderData: keepPreviousData,
   });
 
@@ -74,14 +173,16 @@ export default function App() {
     return <div className={css.loader}>Loading...</div>;
   }
 
-  const posts = data || [];
-
-  const totalPages = posts.length > 0 ? 10 : 0;
+  const totalPages = data.length > 0 ? 10 : 0;
 
   return (
     <div className={css.app}>
       <header className={css.toolbar}>
-        <SearchBox value={searchQuery} onChange={handleSearch} />
+        <SearchBox
+          value={searchQuery}
+          onChange={debouncedSearchQuery}
+        />
+
         {totalPages > 1 && (
           <Pagination
             currentPage={currentPage}
@@ -89,6 +190,7 @@ export default function App() {
             onPageChange={setCurrentPage}
           />
         )}
+
         <button className={css.button} onClick={openCreateModal}>
           Create post
         </button>
@@ -102,7 +204,7 @@ export default function App() {
       </Modal>
 
       <PostList
-        posts={posts}
+        posts={data}
         toggleModal={openEditModal}
         toggleEditPost={selectPostForEdit}
       />
--- src/services/postService.ts
+++ src/services/postService.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import type { Post, NewPost, UpdatePost } from "../types/post";
+import type { NewPost, Post, UpdatePost } from "../types/post";
 
 axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
 
@@ -9,45 +9,40 @@ interface FetchPostsParams {
   limit?: number;
 }
 
-// export const fetchPosts = async (searchText, page) => {};
-// Отримання списку постів з опційним пошуком і пагінацією
+export type FetchPostsResponse = Post[];
+
 export const fetchPosts = async ({
   searchText = "",
   page = 1,
   limit = 10,
-}: FetchPostsParams): Promise<Post[]> => {
-  const params: Record<string, string | number> = {
-    _page: page,
-    _limit: limit,
-  };
-
-  if (searchText.trim()) {
-    params.q = searchText.trim();
-  }
-
-  const { data } = await axios.get<Post[]>("/posts", { params });
-  return data;
+}: FetchPostsParams): Promise<FetchPostsResponse> => {
+  const response = await axios.get<FetchPostsResponse>("/posts", {
+    params: {
+      q: searchText,
+      _page: page,
+      _limit: limit,
+    },
+  });
+  return response.data;
 };
 
-
-// export const createPost = async (newPost) => {};
 export const createPost = async (newPost: NewPost): Promise<Post> => {
-  const { data } = await axios.post<Post>("/posts", newPost);
-  return data;
+  const response = await axios.post<Post>("/posts", newPost);
+  return response.data;
 };
 
-
-// export const editPost = async (newDataPost) => {};
 export const editPost = async (
   postId: number,
-  updatedData: UpdatePost
+  newDataPost: UpdatePost
 ): Promise<Post> => {
-  const { data } = await axios.patch<Post>(`/posts/${postId}`, updatedData);
-  return data;
+  const response = await axios.patch<Post>(`/posts/${postId}`, newDataPost);
+  return response.data;
 };
 
-
-// export const deletePost = async (postId) => {};
 export const deletePost = async (postId: number): Promise<void> => {
-  await axios.delete(`/posts/${postId}`);
+  const response = await axios.delete(`/posts/${postId}`);
+  return response.data;
 };
+
+
+
